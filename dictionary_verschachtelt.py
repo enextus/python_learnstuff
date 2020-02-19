76b6acdab9dc0d685fd3b1265fd2a3ebe72c6dae
enextus <enextus@gmail.com>
2020-02-19 14:12:55 +0100

    upd

--- dictionary_verschachtelt.py
+++ dictionary_verschachtelt.py
@@ -3,43 +3,44 @@
 language = ['D', 'F', 'E', 'd', 'f', 'e']
 
 while True:
-    a = input('Wähle das Herkunftsprache aus: In dem Du die Buchstabe D, F oder E: ')
+    a = input('Wähle das Herkunftsprache aus: In dem Du die Buchstabe D, F, E: ')
     if a in language:
         break
     else:
-        print("bitte nur D,F,E: ")
+        print("bitte nur D,F,E,I: ")
 
 while True:
-    z = input('Wähle das Zielsprache aus: In dem Du die Buchstabe D, F oder E: ')
+    z = input('Wähle das Zielsprache aus: In dem Du die Buchstabe D, F, E: ')
     if z in language:
         break
     else:
         print("bitte nur D,F,E: ")
 
 while True:
+    print(f"test")
     wort = input('Bitte das Word eingeben: ')
     if wort.isdigit():
         print("bitte nur Wörter eingeben! ")
     else:
         break
 
+
 a, z, wort = a.lower(), z.lower(),  wort.lower()
 
-ed = {"red" : "rot", "green" : "grün", "blue" : "blau", "yellow":"gelb"}
-df = {"rot" : "rouge", "grün" : "vert", "blau" : "bleu", "gelb":"jaune"}
+e_d = {"red" : "rot", "green" : "grün", "blue" : "blau", "yellow":"gelb"}
+d_f = {"rot" : "rouge", "grün" : "vert", "blau" : "bleu", "gelb":"jaune"}
 
-temp = a+z
+temp = a + '_' + z
 
-if temp == "ed":
-    translate = ed[wort]
-elif temp == "df":
-    translate = df[wort]
-elif temp == "ef":
-    translate = df[ed[wort]]
-elif temp == "de":
-    translate = df[wort]
+if temp == "e_d":
+    translate = e_d[wort]
+elif temp == "d_f":
+    translate = d_f[wort]
+elif temp == "e_f":
+    translate = d_f[e_d[wort]]
+elif temp == "d_e":
+    translate = d_f[wort]
 
 
 print('Das Herkunftsprache ist', a, 'Das Wort ist', " \" "+wort+" \" ", 'und das Zielsprache ist', z,".")
 print("Die Übersetzung lautet: ", translate)
-
